955bf4d0687b0850d56c2b0b2d9fcf80fef47b8b
Unknown <Kim.swope@yahoo.com>
2018-06-16 21:45:18 -0400

    moved old app files to new app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    points: 0, 
+    friends
+  };
+
+  countPoint = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    let total = this.state.points +1;
+    this.setState({points:total});
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Navbar/>
+        <Title/>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            countPoint={this.countPoint}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Navbar.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+const Navbar = props => (
+  <nav>
+    <ul>
+      <li className="brand animated lightSpeedIn">
+        <a href="/clicky-game/">{props.title}</a>
+      </li>
+
+      <li id="rw">{props.rightWrong}</li>
+
+      <li id="cur-sco">Current Score: {props.score}</li>
+
+      <li id="top-sco">Top Score: {props.topScore}</li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
