2bffc5ec7a9d5e2aeb59446c48d2017967c3aba5
Unknown <Kim.swope@yahoo.com>
2018-06-16 22:08:05 -0400

    changes to page header & title

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -5,6 +5,7 @@ const Title = props =>
 <div className="title">
 <header>
     <h1>Clicky Game</h1>
+    <h4>Click on an image to earn points, but don't click on an image more than once!</h4>
 </header>
     
 </div>
